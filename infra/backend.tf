7a68914aa0d3f63a250deec3ad21835323406b35
Lucas Henrique <lucashsilvaa8@gmail.com>
2025-09-24 23:23:12 -0300

    Update backend.tf

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,8 +1,8 @@
 terraform {
-    backend "azurerm" {
-        resource_group_name = "rg-tfstate"
-        storage_account_name = "tfstatecurso4124"
-        container_name = "tfstate"
-        key = "terraform.tfstate"
-    }
+  backend "azurerm" {
+    resource_group_name  = "rg-tfstate"
+    storage_account_name = "tfstatecurso4124"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
 }
